d9f5af91d1a49d09d8fd174ff4a5743cf8149896
Sarthak0967 <sarthakdoc09@gmail.com>
2025-09-08 09:49:46 +0530

    feat: implement TRPC caller for server-side API calls in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-"use client"
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+// "use client"
+// import { useTRPC } from "@/trpc/client";
+// import { useQuery } from "@tanstack/react-query";
 // import Image from "next/image";
 
-export default function Home() {
-  const trpc = useTRPC();
+import { caller } from "@/trpc/server";
+
+export default async function Home() {
+  // const trpc = useTRPC();
+
+  const data = await caller.hello({ text: "Sarthak"});
   
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "Sarthak"}));
+  // const { data } = useQuery(trpc.hello.queryOptions({ text: "Sarthak"}));
   return (
       <div>
         {JSON.stringify(data)}
